89af71457b3338f112f41797d729fbf231ae5291
Lavrentev Arseny <22660769+lavrentev@users.noreply.github.com>
2020-10-21 16:53:35 +0300

    Add files via upload

---
+++ decode_abdul_utf8.py
@@ -0,0 +1,47 @@
+#! py3
+'''make abdul transliterated again'''
+import os
+from unidecode import unidecode
+
+EXTENSION = ".mtl"
+SUFFIX = "_transl"
+
+
+dirpath = os.getcwd()
+for file in os.listdir(dirpath):
+    if file.endswith(EXTENSION) and not file.endswith(SUFFIX + EXTENSION):
+        INPUT_FILE_PATH = os.path.join(dirpath, file)
+        OBJ_NAME = file[:file.rfind(".")]
+
+OUTPUT_FILE_PATH = INPUT_FILE_PATH[:INPUT_FILE_PATH.rfind(".")] + SUFFIX + INPUT_FILE_PATH[INPUT_FILE_PATH.rfind("."):]
+
+with open(INPUT_FILE_PATH, encoding="utf_8") as ifd:
+    with open(OUTPUT_FILE_PATH, 'w') as ofd:
+        for string in ifd:
+            ofd.write(unidecode(string))
+
+
+EXTENSION = ".obj"
+
+dirpath = os.getcwd()
+for file in os.listdir(dirpath):
+    if file.endswith(EXTENSION) and not file.endswith(SUFFIX + EXTENSION):
+        INPUT_FILE_PATH = os.path.join(dirpath, file)
+        OBJ_NAME = file[:file.rfind(".")]
+
+OUTPUT_FILE_PATH = INPUT_FILE_PATH[:INPUT_FILE_PATH.rfind(".")] + SUFFIX + INPUT_FILE_PATH[INPUT_FILE_PATH.rfind("."):]
+
+with open(INPUT_FILE_PATH, encoding="utf_8") as ifd:
+    with open(OUTPUT_FILE_PATH, 'w') as ofd:
+        for string in ifd:
+            ofd.write(unidecode(string))
+
+
+
+TEX_FOLDER = dirpath + "\\" + OBJ_NAME
+if not os.path.isdir(TEX_FOLDER + "_Texture") and os.path.isdir(TEX_FOLDER + "_Текстура"):
+    os.rename(TEX_FOLDER + "_Текстура", TEX_FOLDER + "_Texture")
+TEX_FOLDER = TEX_FOLDER + "_Texture"
+
+for file in os.listdir(TEX_FOLDER):
+    os.rename(os.path.join(TEX_FOLDER, file), os.path.join(TEX_FOLDER, unidecode(file)))
